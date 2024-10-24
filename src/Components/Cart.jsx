7b259999455777b65cf60a3be2c0ef20d959dfff
ramsha1993 <rimshasaleem1993@gmail.com>
2024-10-24 14:26:23 -0700

    change

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,24 +4,17 @@ import { remove } from '../store/cartSlice';
 import { useDispatch } from 'react-redux';
 
 const Cart = () => {
-const [item, setitem] = useState([])
+const [item,setitem] = useState([])
     const dispatch=useDispatch();
 
     const products=useSelector((state)=>state.cart)
 
 const updatelocalstorage=(items)=>{
-const existingData=localStorage.getItem('data')
+    console.log(items)
 let updateData=[];
-if(existingData) 
-    {try{
-    updateData=JSON.parse(existingData)
-
-}catch(error){
-    console.log("error",error)
-}}
 updateData=[...updateData,...items]
-const filter=updateData.filter(existingData.id === items.id)
-localStorage.setItem('data',JSON.stringify(filter))
+// console.log(updateData)
+localStorage.setItem('data',JSON.stringify(updateData))
 }
 
  useEffect(()=>{
@@ -31,24 +24,28 @@ localStorage.setItem('data',JSON.stringify(filter))
     }
  },[products]) 
 
-const handleRemove=(item)=>{
-        dispatch(remove(item))
-
-}
+const handleRemove=(elem)=>{
+        dispatch(remove(elem))
+        console.log(elem)
+        const localData = JSON.parse(localStorage.getItem('data')) || [];
+        const updatedData = localData.filter((item) => item.aa !== elem.aa); // Assuming 'aa' is a unique identifier
+        localStorage.setItem('data', JSON.stringify(updatedData));
+    
+        setitem(updatedData)
+        }
 
 useEffect(()=>{
-const localdata=localStorage.getItem('data')
+ const localdata=JSON.parse(localStorage.getItem('data'))
 if(localdata){
 try{
-    const parsedata=JSON.parse(localdata)
-    setitem(parsedata)
-}catch(error){
+    
+setitem(localdata)}
+catch(error){
 console.log('Error',error)
 }}
 }
 
 ,[])
-
 
 
    
@@ -57,17 +54,29 @@ console.log('Error',error)
 
 
 
-  return (<>
-    <div><h1 style={{textAlign:'center'}}>Cart</h1></div>
-    <div className="cart-wrapper">
-{item.map((element,index)=>(
-    <div className='cart-pro'key={index}><img width='100px' height='100px'src={element.image} alt="#" />
-    <h5>{element.aa}</h5><h5 style={{width:'100px'}}>{element.price}</h5><button className='but'onClick={(()=>handleRemove(element))}>Remove</button></div>
-
-)
-    
-    
-    )}    
-</div>
-        </>)}
+return (
+    <>
+      <div><h1 style={{ textAlign: 'center' }}>Cart</h1></div>
+      <div className="cart-wrapper">
+        {item.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          item.map((element, index) => (
+            <div className='cart-pro' key={index}>
+              <img
+                width='100px'
+                height='100px'
+                src={element.image || "/static/media/headphone.35744d1d7bc0b01d5a3d.jpg"}
+                alt="#"
+              />
+              <h5>{element.aa}</h5>
+              <h5 style={{ width: '100px' }}>{element.price}</h5>
+              <button className='but' onClick={() => handleRemove(element)}>Remove</button>
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
+}
 export default Cart
